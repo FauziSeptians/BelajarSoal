f1130c5aae7034573ca839dbf39eb47a2ce587c6
Muhammad Fauzi Septiana Putra <114633811+FauziSeptians@users.noreply.github.com>
2025-02-02 22:19:41 +0700

    Time: 0 ms (100.00%) | Memory: 48.4 MB (91.49%) - LeetSync

--- 66-plus-one/plus-one.js
+++ 66-plus-one/plus-one.js
@@ -6,7 +6,5 @@ var plusOne = function(digits) {
     const digitVal = BigInt(digits.join(""));
     const result = digitVal + BigInt(1);
 
-    console.log(digitVal);
-
     return result.toString().split("").map(Number);
 };
