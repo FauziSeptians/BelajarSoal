7f2d01fd91685fa041abb49942ae8c047ccd64f1
Muhammad Fauzi Septiana Putra <114633811+FauziSeptians@users.noreply.github.com>
2025-02-08 17:44:14 +0700

    Time: 13 ms (62.03%) | Memory: 63.1 MB (24.71%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const seen = new Set();
+
+    for (let i = 0; i < nums.length; i++) {
+        if (seen.has(nums[i])) {
+            return true; // Duplicate found
+        }
+        seen.add(nums[i]);
+    }
+
+    return false; // No duplicates found
+};
